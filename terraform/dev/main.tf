bea49c5e93901ec168747115515a77abbd9aa85e
Kasarapu Aravind <splendidaravind552@gmail.com>
2025-06-11 00:02:03 -0400

    code

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -16,7 +16,8 @@ resource "aws_security_group" "mysg" {
 resource "aws_vpc_security_group_egress_rule" "mysgrule1" {
   security_group_id = aws_security_group.mysg.id
   cidr_ipv4 = "0.0.0.0/0"
-  to_port = "22"
+  from_port = 22
+  to_port = 22
   ip_protocol = "tcp"
   tags = {
     Name = "mysg"
